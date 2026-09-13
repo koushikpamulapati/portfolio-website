c598fee9d49264df7e217e85dc85e6e1c31ab293
koushikpamulapati <koushikpamulapati123@gmail.com>
2026-09-14 00:14:50 +0530

    Connect Flask to TiDB Cloud

--- app.py
+++ app.py
@@ -10,9 +10,13 @@ app = Flask(__name__)
 
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
+    database=os.getenv("DB_NAME"),
+    ssl_ca=os.getenv("DB_CA_PATH"),
+    ssl_verify_cert=True,
+    ssl_verify_identity=True
 )
 
 
